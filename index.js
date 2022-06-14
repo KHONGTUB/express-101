1841b0fff9fe556f7bf5013caaafffb235422872
KHONGTUB <jkojeda12@gmail.com>
2022-06-13 23:38:17 -0500

    Finished put and delete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const people = require("./data");
+let people = require("./data");
 
 const PORT = 8000;
 
@@ -29,5 +29,36 @@ app.post("/people", (req, res) => {
   people.push(person);
   res.json(person);
 });
+
+app.put("/people/:id", (req, res) =>{
+  const id = req.params.id
+  const person = people.find((person) => person.id === Number(id))
+  const foundindex = people.findIndex((person) => person.id === Number(id))
+  const newinfo = {
+    
+    ...person,
+    ...req.body
+
+  }
+
+  people.splice(foundindex, 1, newinfo)
+  res.json(newinfo)
+})
+
+app.delete("/people/:id", (req, res) => {
+  const id = req.params.id
+  const foundindex = people.findIndex((person) => person.id === Number(id))
+
+  people = people.filter(people => {
+    return people.id !== Number(id)
+  })
+  for( let i = 0; i < people.length; i++){
+
+    if(people[i].id > Number(id)){
+      people[i].id = (people[i].id - 1)
+    }
+  }
+  res.json(people)
+})
 
 app.listen(PORT, () => console.log(`I am listening on port ${PORT}`));
